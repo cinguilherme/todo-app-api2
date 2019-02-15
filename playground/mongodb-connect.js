f4a9ef6d865b38d9030b975b7be7de15f100116c
cinguilherme <cinguilherme@gmail.com>
2019-02-15 15:26:05 -0200

    ready to move on into developing the api

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -9,24 +9,13 @@ const callBack = (err, dbObject) => {
   }
   log('connected to the mongodb server.');
 
-  const todo = {
-    text: 'To check if I can write stuff into mongodb',
-    done: false,
-  };
-
-  const user = {
-    name: 'Guilherme',
-    age: 32,
-    location: 'recife',
-  };
-
   dbObject.collection('Users').find({
     name: 'Guilherme',
   }).count().then((count) => {
     log(count);
   });
 
-  dbObject.close();
+  return dbObject.close();
 };
 
 MongoClient.connect(mongoUrl, callBack);
