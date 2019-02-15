2a52280665cb657482e3ed5750303a452e95424f
cinguilherme <cinguilherme@gmail.com>
2019-02-15 15:20:45 -0200

    setup dev enviroment

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,49 +1,32 @@
-const { MongoClient, ObjectID } = require('mongodb');
-const log = console.info;
+/* eslint-disable no-undef */
+const { MongoClient } = require('mongodb');
 
-//mongo db protocol -> mongodb:// At Port and db name
-const mongoUrl = `mongodb://localhost:27017/TodoApp`;
+const mongoUrl = 'mongodb://localhost:27017/TodoApp';
 
-//MONGO V2
 const callBack = (err, dbObject) => {
-    if (err) {
-        return log('Unnable to connect to db');
-    }
-    log('connected to the mongodb server.');
+  if (err) {
+    return log('Unnable to connect to db');
+  }
+  log('connected to the mongodb server.');
 
-    const todo = {
-        text: 'To check if I can write stuff into mongodb',
-        done: false,
-    };
+  const todo = {
+    text: 'To check if I can write stuff into mongodb',
+    done: false,
+  };
 
-    const user = {
-        name: 'Guilherme',
-        age: 32,
-        location: 'recife',
-    };
+  const user = {
+    name: 'Guilherme',
+    age: 32,
+    location: 'recife',
+  };
 
-    // dbObject.collection('Todos').insertOne(todo, (err, result) => {
-    //     if (err) {
-    //         return log('Unnable to create todo.');
-    //     }
-    //     log('Todo created successfully');
-    //     log(JSON.stringify(result.ops, undefined, 2));
-    // })
+  dbObject.collection('Users').find({
+    name: 'Guilherme',
+  }).count().then((count) => {
+    log(count);
+  });
 
-    // dbObject.collection('Users').insertOne(user, (err, result) => {
-    //     if (err) {
-    //         return log('Unnable to insert User');
-    //     }
-    //     log(JSON.stringify(result.ops, undefined, 2));
-    // });
-
-    dbObject.collection('Users').find({
-        name: 'Guilherme'
-    }).count().then((count) => {
-        log(count);
-    });
-
-    dbObject.close();
+  dbObject.close();
 };
 
 MongoClient.connect(mongoUrl, callBack);
